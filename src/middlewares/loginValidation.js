52fe52eb3f04f00af3cc0ab68cd00bfa11ddfa2b
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2022-08-30 16:52:53 -0300

    requisito 4

--- src/middlewares/loginValidation.js
+++ src/middlewares/loginValidation.js
@@ -2,11 +2,17 @@ const loginValidation = (req, res, next) => {
 const { email } = req.body;
 const { password } = req.body;
 const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-if (!email || !emailRegex.test(email)) {
-res.status(400).json({ message: 'informe um email real' });
+if (!email) {
+res.status(400).json({ message: 'O campo "email" é obrigatório' });
 }
-if (!password || password <= 6) {
-res.status(400).json({ message: 'a senha deve ter 6 ou mais caracteres' });
+if (!emailRegex.test(email)) {
+res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+}
+if (!password) {
+res.status(400).json({ message: 'O campo "password" é obrigatório' });
+}
+if (password.length < 6) {
+res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
 }
 next();
 };
